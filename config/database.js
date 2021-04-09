473623eea0b97aa90e0f46df9d3eaff571c08965
Michel Lima Ribeiro <michelribeiro2004@gmail.com>
2021-04-09 09:13:59 -0300

    updating info

--- config/database.js
+++ config/database.js
@@ -1,20 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'cluster0-aeenb.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'odontopaz_data'),
-        username: env('DATABASE_USERNAME', 'michel'),
-        password: env('DATABASE_PASSWORD', 'd16m08a1981'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'handlescms'),
+          username: env('DATABASE_USERNAME', 'michelribeiro'),
+          password: env('DATABASE_PASSWORD', '17set04'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        options: {}
       },
     },
-  },
-});
+  });
